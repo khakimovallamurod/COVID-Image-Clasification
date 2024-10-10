7a1a0ddb734c9febc6694088c82acf4590e663ce
codeschoolacademy <codeschoolacademy@gmail.com>
2024-10-10 15:05:41 +0500

    train model

--- train_model.py
+++ train_model.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO  # type: ignore
 
 # Load model
-model = YOLO('yolov8n-cls.pt')
+model = YOLO('yolov8n-cls.pt') # Yolo modeli
 model.train(
-    data = 'dataset',
-    epochs = 300, 
-    imgsz = 224
+    data = 'dataset', # Your dataset path
+    epochs = 300, # Epochs count
+    imgsz = 224 # Images size 
 )
